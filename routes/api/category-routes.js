c5022a82ca8951f48d5b11e9cf1d2e68ac570137
Jeremy O'Daniel <jeremy.odaniel@gmail.com>
2022-05-26 13:43:22 -0500

    Add CRUD routes to category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -12,30 +12,55 @@ router.get('/', (req, res) => {
   .then(categories => {
     res.json(categories)
   });
+  // ADD CATCH STATEMENT
 });
 
-// GET /api/categories/1
+// GET /api/categories/"id"
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   Category.findOne({
     where: {
       id: req.params.id
     },
-
+    // be sure to include its associated Products
   })
-  // be sure to include its associated Products
+  .then(categories => {
+    res.json(categories)
+  });
+  // ADD CATCH STATEMENT
 });
 
+// CREATE /api/categories
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+    .then(categories => res.json(categories))
+    // ADD CATCH STATEMENT
 });
 
+// UPDATE /api/categories/"id"
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(categories => res.json(categories))
+  // ADD CATCH STATEMENT 
 });
 
+// DELETE /api/categories/"id"
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(categories => {
+    res.json(categories)
+  })
 });
 
 module.exports = router;
